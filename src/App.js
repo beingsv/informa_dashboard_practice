edeff31403f9ff0746da54e66cc32e29f4f72ccd
Vipul Kumar <vipulmittal48@gmail.com>
2023-11-08 22:10:58 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,244 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="frame">
+      <div className="frame1">
+        <div>Informa OCC SCM</div>
+        <div className="frame2"></div>
+      </div>
+      <div className="frame3">
+        <div className="module1">fgfgfgfg</div>
+        <div className="module1">bgbgbgggbgb</div>
+      </div>
+      <div className="frame3">
+        <div className="module2">
+          <div className="heading">
+            <h3>Prod Frontend-A</h3>
+          </div>
+          <div className="module2-inner">
+            <div className="error-inbox">
+              <p>ERROR INBOX</p>
+              <h3>5</h3>
+            </div>
+
+            <div className="error-inbox slo">
+              <p>SLI/SLO</p>
+              <h3>00/00</h3>
+            </div>
+            <div className="error-inbox disruption">
+              <p>DISRUPTIONS</p>
+              <h3>5</h3>
+            </div>
+          </div>
+          <div className="scores">
+            <div className="scores-row">
+              <div className="apdex">
+                <div>apdex</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+              <div className="apdex">
+                <div>Throuhput</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+            </div>
+            {/* <div className="scores-row">vgvgv</div>
+             */}
+            <div className="scores-row">
+              <div className="apdex">
+                <div>Error Rate</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+              <div className="apdex">
+                <div>Slowest Transactions</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="module2">
+          <div className="heading">
+            <h3>Prod Frontend-B</h3>
+          </div>
+          <div className="module2-inner">
+            <div className="error-inbox">
+              <p>ERROR INBOX</p>
+              <h3>5</h3>
+            </div>
+
+            <div className="error-inbox slo">
+              <p>SLI/SLO</p>
+              <h3>00/00</h3>
+            </div>
+            <div className="error-inbox disruption">
+              <p>DISRUPTIONS</p>
+              <h3>5</h3>
+            </div>
+          </div>
+          <div className="scores">
+            <div className="scores-row">
+              <div className="apdex">
+                <div>apdex</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+              <div className="apdex">
+                <div>Throuhput</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+            </div>
+            {/* <div className="scores-row">vgvgv</div>
+             */}
+            <div className="scores-row">
+              <div className="apdex">
+                <div>Error Rate</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+              <div className="apdex">
+                <div>Slowest Transactions</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="module2">
+          <div className="heading">
+            <h3>Prod Frontend-C</h3>
+          </div>
+          <div className="module2-inner">
+            <div className="error-inbox">
+              <p>ERROR INBOX</p>
+              <h3>5</h3>
+            </div>
+
+            <div className="error-inbox slo">
+              <p>SLI/SLO</p>
+              <h3>00/00</h3>
+            </div>
+            <div className="error-inbox disruption">
+              <p>DISRUPTIONS</p>
+              <h3>5</h3>
+            </div>
+          </div>
+          <div className="scores">
+            <div className="scores-row">
+              <div className="apdex">
+                <div>apdex</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+              <div className="apdex">
+                <div>Throuhput</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+            </div>
+            {/* <div className="scores-row">vgvgv</div>
+             */}
+            <div className="scores-row">
+              <div className="apdex">
+                <div>Error Rate</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+              <div className="apdex">
+                <div>Slowest Transactions</div>
+                <div className="apdex-score">0.2</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="module2">rrrrrrrr</div> */}
+      </div>
+      <div className="frame4">
+        <div className="frame4-inner1">
+          <div className="frame4-module">
+            <div className="heading">
+              <h3>API Gateway</h3>
+            </div>
+            <div className="module2-inner">
+              <div className="error-inbox">
+                <p>ERROR INBOX</p>
+                <h3>5</h3>
+              </div>
+
+              <div className="error-inbox slo">
+                <p>SLI/SLO</p>
+                <h3>00/00</h3>
+              </div>
+              <div className="error-inbox disruption">
+                <p>DISRUPTIONS</p>
+                <h3>5</h3>
+              </div>
+            </div>
+            <div className="scores">
+              <div className="scores-row">
+                <div className="apdex">
+                  <div>apdex</div>
+                  <div className="apdex-score">0.2</div>
+                </div>
+                <div className="apdex">
+                  <div>Throuhput</div>
+                  <div className="apdex-score">0.2</div>
+                </div>
+              </div>
+              {/* <div className="scores-row">vgvgv</div>
+               */}
+              <div className="scores-row">
+                <div className="apdex">
+                  <div>Error Rate</div>
+                  <div className="apdex-score">0.2</div>
+                </div>
+                <div className="apdex">
+                  <div>Slowest Transactions</div>
+                  <div className="apdex-score">0.2</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame4-module">
+            <div className="heading">
+              <h3>Lambda</h3>
+            </div>
+            <div className="module2-inner">
+              <div className="error-inbox">
+                <p>ERROR INBOX</p>
+                <h3>5</h3>
+              </div>
+
+              <div className="error-inbox slo">
+                <p>SLI/SLO</p>
+                <h3>00/00</h3>
+              </div>
+              <div className="error-inbox disruption">
+                <p>DISRUPTIONS</p>
+                <h3>5</h3>
+              </div>
+            </div>
+            <div className="scores">
+              <div className="scores-row">
+                <div className="apdex">
+                  <div>apdex</div>
+                  <div className="apdex-score">0.2</div>
+                </div>
+                <div className="apdex">
+                  <div>Throuhput</div>
+                  <div className="apdex-score">0.2</div>
+                </div>
+              </div>
+              {/* <div className="scores-row">vgvgv</div>
+               */}
+              <div className="scores-row">
+                <div className="apdex">
+                  <div>Error Rate</div>
+                  <div className="apdex-score">0.2</div>
+                </div>
+                <div className="apdex">
+                  <div>Slowest Transactions</div>
+                  <div className="apdex-score">0.2</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
